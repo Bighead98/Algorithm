4d2853d1af07b3059138419e92512c22f0a2f181
bighead98 <dearshawn@naver.com>
2020-10-04 15:12:27 +0900

    백준 1967 트리의 지름 골드 4 문제 완료

--- test.cpp
+++ test.cpp
@@ -1,37 +1,54 @@
-//19572 small
+//1967 트리의 지름
 
 #include <iostream>
-
+#include <vector>
+#include <string.h>
 using namespace std;
 
-void f(int a, int b, int c)
+vector<pair<int,int> > node[100001];
+int visited[100001]={0};
+int n;
+int ans=0;
+int end_point=0;
+void dfs(int point, int length)
 {
-    double s = double(a+b+c)/2;
-    double a1,b1,c1;
-    if(s>a && s>b && s>c)
-    {
-        a1=s-c;
-        b1=s-b;
-        c1=s-a;
-        cout << 1 << endl;
-        cout << fixed;
-        cout.precision(1);
-        cout << a1 << " " << b1 
-        << " " << c1 << " " ;
-    }
-    else
-    {
-        cout << "-1";
-    }
+    if(visited[point])
+        return;
     
-}
+    visited[point] = 1;
+    if(ans<length)
+    {
+        ans=length;
+        end_point=point;
+    }
 
+    for(int i=0;i<node[point].size();i++)
+    {
+        dfs(node[point][i].first,length+node[point][i].second);
+    }
+
+}
 int main()
 {
-    int a,b,c;
-    cin >> a >> b >> c;
+    int n;
+    cin >> n;
+    int a,b;
+    int parent,child,length;
+    for(int i=0;i<n-1;i++)
+    {
+        cin >> parent >> child >> length;
+        node[parent].push_back(make_pair(child,length));
+        node[child].push_back(make_pair(parent,length));
+    }
 
-    f(a,b,c);
+    dfs(1,0);
+    a=end_point;
+    ans = 0;
+    memset(visited,0,sizeof(visited));
+    dfs(end_point,0);
 
+    b=end_point;
+    cout << ans << endl;
+    // << " " << a << " " << b<<endl;
     return 0;
 }
